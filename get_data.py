a509b5784a560228538687bfbe56201df5c02764
ewe08 <wileere@yandex.ru>
2023-11-03 19:10:03 +0300

    create PDF

--- get_data.py
+++ get_data.py
@@ -1,15 +1,18 @@
 import json
 
-f = open('input.json')
-input_data = json.load(f)
+def get_data(json_name: str):
+    f = open(json_name)
+    input_data = json.load(f)
 
-data = {
-    'Автор(ы) научной статьи': input_data['name'],
-    'Научное издание': input_data['magazine'],
-    'Выпуск издания': f"{input_data['number']} выпуск {input_data['year']} года",
-    'Направление': input_data['chapter'],
-    'Секция': '-',
-    'Название научной статьи': input_data['topic'],
-    'Научный руководитель': input_data['assistant'],
-    'Ориентировочная дата опубликования': input_data['date_publication'],
-}
+    data = {
+        'Автор(ы) научной статьи': input_data['name'],
+        'Научное издание': input_data['magazine'],
+        'Выпуск издания': f"{input_data['number']} выпуск {input_data['year']} года",
+        'Направление': input_data['chapter'],
+        'Секция': '-',
+        'Название научной статьи': input_data['topic'],
+        'Научный руководитель': input_data['assistant'],
+        'Ориентировочная дата опубликования': input_data['date_publication'],
+    }
+
+    return data.items()
--- main.py
+++ main.py
@@ -1,24 +1,64 @@
-# -*- coding: utf-8 -*-
-import fitz
+from datetime import date
 
-WIDTH = 1200
-HEIGHT = 1700
+from fpdf import FPDF
+from fpdf.fonts import FontFace
 
-doc = fitz.open()
-page = doc.new_page(0, WIDTH, HEIGHT)
+from get_data import get_data
 
-page_index = 0
-rect = (0, 0, WIDTH, HEIGHT)
-imgpath = 'background.png'
-page = doc[page_index]
-page.insert_image(rect, filename=imgpath)
+class PDF(FPDF):
+    def header(self):
+        self.set_font("Roboto", "", 10)
 
-page.insert_font(fontname='Roboto-Bold', fontfile='./fonts/Roboto-Bold.ttf')
-shape = page.new_shape()
-header = "                       Справка\nо принятии статьи к публикации"
-shape.insert_text((450,200), header, fontname='Roboto-Bold', fontsize=24)
-shape.commit()
+        self.ln(20)
+        self.cell(80)
+        self.cell(30, 10, "Справка", align="C")
+        
+        self.ln(7)
+        self.cell(80)
+        self.cell(30, 10, "о принятии статьи к публикации", align="C")
+
+        self.ln(15)
+
+        
+    def chapter_body(self, data):
+        pdf.set_font("Roboto", "B", size=10)
+        with pdf.table(width=150, headings_style=headings_style) as table:
+            for data_row in data:
+                row = table.row()
+                for datum in data_row:
+                    row.cell(datum, align="C", padding=(0, 0, 5, 0))
+        self.ln(10)
+    
+    def chapter_footer(self):
+        pdf.set_font("Roboto", "", size=9)
+
+        self.cell(19)
+        self.cell(30, 10, "Дата:", align="L")
+        self.ln(5)
+        self.cell(19)
+        self.cell(30, 10, f"{date.today().strftime('%d.%m.%Y')} г.")
+        self.ln(10)
+
+        d = [["Представитель Научно-издательской платформы «Из уст»", "\n\n____________________________/Зарипов Ш.Р."]]
+        with pdf.table(width=150, headings_style=headings_style) as table:
+            for data_row in d:
+                row = table.row()
+                for datum in data_row:
+                    row.cell(datum, align="l")
+        self.ln(10)
 
 
+pdf = PDF()
+pdf.set_page_background("background.png")
+pdf.add_font("Roboto", style="", fname="./fonts/Roboto-Medium.ttf")
+pdf.add_font("Roboto", style="B", fname="./fonts/Roboto-Bold.ttf")
+headings_style = FontFace()
 
-doc.save('output.pdf')
+pdf.add_page()
+
+data = get_data('input.json')
+pdf.chapter_body(data)
+
+pdf.chapter_footer()
+
+pdf.output("./outputs/output.pdf")
